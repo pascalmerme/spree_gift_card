fbfb9cde51e10e28a9af8024183a6ed5f90f3ad6
Tom Russell <tom@cishub.co.uk>
2015-03-04 21:29:57 +0000

    Make seed script more idempotent
    The seed script when run should be able to be rerun as many times as
    necessary.
    
    Not quite there yet (it'll still create multiple variants and so on) but
    we're nearly able to remove the if guard. This gets us closer to that
    goal.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,11 @@ if Spree::Product.gift_cards.count == 0
   method.save
 
   product = Spree::Product.new(available_on: Time.now, name: "Gift Card", is_gift_card: true, slug: 'gift-card', price: 0, shipping_category_id: shipping_category.id)
-  option_type = Spree::OptionType.new(name: "is-gift-card", presentation: "Value")
+
+  option_type = Spree::OptionType.create_with(name: "is-gift-card", presentation: "Value").find_or_create_by(name: "is-gift-card")
   product.option_types << option_type
   [25, 50, 75, 100].each do |value|
-    option_value = Spree::OptionValue.new(name: value, presentation: "$#{value}")
+    option_value = Spree::OptionValue.create_with(presentation: "$#{value}").find_or_create_by(name: value.to_s)
     option_value.option_type = option_type
     opts = { price: value.to_i, sku: "GIFTCERT#{value}" }
     variant = Spree::Variant.new(opts)

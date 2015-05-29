fbbae145a2ee71636ffa786cddb81339147d5b3b
Phil Battos <philbattos@gmail.com>
2015-05-29 15:57:05 -0600

    update durable decorator sha

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,6 +1,6 @@
 Spree::OrderContents.class_eval do
 
-  durably_decorate :grab_line_item_by_variant, mode: 'soft', sha: '5606323a3e50e650b2ad1a51c43ba54ebc469b72' do |variant, *args|
+  durably_decorate :grab_line_item_by_variant, mode: 'soft', sha: '173bcb9f7515b32b2ad34c3ce4a31a004db96f20' do |variant, *args|
     raise_error = args[0] || false
 
     if variant.product.is_gift_card?

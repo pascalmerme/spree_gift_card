d22b8a86d19e4fa823fbfe1d4ddd3fda6bb7cb7b
Pascal <pascalmerme@no-log.org>
2015-10-08 10:12:59 +0200

    fix double variants bug

--- app/controllers/spree/gift_cards_controller.rb
+++ app/controllers/spree/gift_cards_controller.rb
@@ -38,7 +38,7 @@ module Spree
 
     def find_gift_card_variants
       gift_card_product_ids = Product.not_deleted.where(is_gift_card: true).pluck(:id)
-      @gift_card_variants = Variant.joins(:prices).where(["amount > 0 AND product_id IN (?) AND is_master = 0", gift_card_product_ids]).order("amount")
+      @gift_card_variants = Variant.joins(:prices).where(["amount > 0 AND product_id IN (?) AND is_master = FALSE", gift_card_product_ids]).order("amount")
     end
 
     def gift_card_params
